2d1b365b03bb26bbcd7478514e336ad63f70191f
Adam <chillcoders@gmail.com>
2022-04-14 02:43:16 -0400

    whiteboard

---
+++ vowel.js
@@ -0,0 +1,3 @@
+// idea 1 - make an array  of vowels,  make a count, if vowel match increase count
+//idea 2- make an array of characers , make a variable count = 0, so splice up an array and then sort it, if it matches, increase count
+// idea 2 cont -if 0 then return 0 matches
