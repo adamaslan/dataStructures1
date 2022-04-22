857c014c38ecaa7c526ab80fe30dd5848ce6ba60
Adam <chillcoders@gmail.com>
2022-04-22 11:13:51 -0400

    ll2-methods

--- linkedlast.js
+++ linkedlast.js
@@ -18,3 +18,32 @@ class Node {
       this.head = new Node(data, this.head);
     }
   
+    // from free code camp
+
+    size() {
+        let count = 0; 
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    getLast() {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        return lastNode
+    }
+
+    getFirst() {
+        return this.head;
+    }
